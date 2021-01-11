673d26697463fa000442d40216803dbf6b236619
Sergey Balashov <Balashov.152@gmail.com>
2021-01-11 11:49:09 +0300

    position view

--- Shared/Views/Home tab/PositionRowView.swift
+++ Shared/Views/Home tab/PositionRowView.swift
@@ -13,33 +13,49 @@ struct PositionRowView: View {
     let position: Position
 
     var body: some View {
+        VStack(alignment: .leading) {
+            positionName
+            HStack {
+                leftStack
+                Spacer()
+                rightStack
+            }.padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+        }
+    }
+
+    var positionName: some View {
         HStack {
-            leftStack
-            Spacer()
-            rightStack
-        }.padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+            Text(position.name.orEmpty + " (" + position.ticker.orEmpty + ")")
+        }
+        .lineLimit(1)
+        .font(.system(size: 17, weight: .bold))
     }
 
     var leftStack: some View {
         VStack(alignment: .leading) {
-            Text(position.name.orEmpty).font(.system(size: 17))
-            HStack(spacing: 2) {
-                if let lots = position.lots {
-                    Text(lots.string)
+            Text("Lots: " + position.lots.string)
+
+            HStack {
+                if let value = position.averagePositionPriceNoNkd {
+                    Text("Primary:")
+                    MoneyText(money: value)
                 }
 
-                if let value = position.averagePositionPriceNoNkd?.value {
-                    Text("-")
-                    Text(value.string(f: ".2"))
+                if let value = position.averagePositionPrice {
+                    Text("Avg:")
+                    MoneyText(money: value)
                 }
+            }
 
-                if let value = position.averagePositionPrice?.value {
-                    Text("-")
-                    Text(value.string(f: ".2"))
+            if let value = position.averagePositionPrice {
+                HStack {
+                    Text("Total:")
+                    MoneyText(money: .init(currency: value.currency,
+                                           value: Double(position.lots) * value.value))
                 }
+            }
 
-            }.font(.system(size: 12))
-        }
+        }.font(.system(size: 12))
     }
 
     var rightStack: some View {
@@ -49,8 +65,8 @@ struct PositionRowView: View {
                     .font(.system(size: 14))
             }
 
-            if let value = position.expectedYield?.value {
-                MoneyText(money: .init(currency: .USD, value: value))
+            if let value = position.expectedYield {
+                MoneyText(money: value)
                     .font(.system(size: 14, weight: .semibold))
             }
         }
